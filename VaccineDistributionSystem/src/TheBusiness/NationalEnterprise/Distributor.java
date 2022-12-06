1bf372be099f8f308acc56fb5a0dbabcf0bba0ac
Freya2597 <dsouza.f@northeastern.edu>
2022-12-05 19:11:08 -0500

    businessModels

--- VaccineDistributionSystem/src/TheBusiness/NationalEnterprise/Distributor.java
+++ VaccineDistributionSystem/src/TheBusiness/NationalEnterprise/Distributor.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package models;
+package TheBusiness.NationalEnterprise;
 
 /**
  *
  * @author Dsouza
  */
-public class Business {
+public class Distributor {
     
 }
