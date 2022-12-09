7602bb2f29e46f8412682f628ce6b40a435cb268
Freya2597 <dsouza.f@northeastern.edu>
2022-12-09 02:52:11 -0500

    Manufacture Dashboard

---
+++ VaccineDistributionSystem/src/TheBusiness/NationalEnterprise/Manufacturer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TheBusiness.NationalEnterprise;
+
+import TheBusiness.Enterprise.Enterprise;
+import TheBusiness.Roles.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dsouza
+ */
+public class Manufacturer extends Enterprise {
+  public Manufacturer(String name) {
+        super(name , EnterpriseType.Manufacturer);
+    }
+
+   
+}
