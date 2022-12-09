596a4462399f6a48d93f01249bfd8322324d40f1
Freya2597 <dsouza.f@northeastern.edu>
2022-12-09 11:49:32 -0500

    OrderVaccine for Provider and Manageorder screen Manufacturer

--- VaccineDistributionSystem/src/userInterface/Provider/OrderVaccine.java
+++ VaccineDistributionSystem/src/userInterface/Provider/OrderVaccine.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userInterface.Manufacture;
+package userInterface.Provider;
 
 import TheBusiness.Ecosystems;
 import TheBusiness.Enterprise.HospitalEnterprise;
