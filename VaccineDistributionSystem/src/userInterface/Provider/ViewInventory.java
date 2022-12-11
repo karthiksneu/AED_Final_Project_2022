b1d8172fc8da0bac1efcfda64563a7b657eac351
Freya2597 <dsouza.f@northeastern.edu>
2022-12-10 19:38:54 -0500

    Manufacturer add vaccine action

--- VaccineDistributionSystem/src/userInterface/Provider/ViewInventory.java
+++ VaccineDistributionSystem/src/userInterface/Provider/ViewInventory.java
@@ -33,6 +33,8 @@ public class ViewInventory extends javax.swing.JPanel {
         jSeparator1 = new javax.swing.JSeparator();
         jLabel2 = new javax.swing.JLabel();
 
+        setBackground(new java.awt.Color(0, 204, 204));
+
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
