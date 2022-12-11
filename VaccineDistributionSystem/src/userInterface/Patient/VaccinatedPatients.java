ddc005903fa128f8dbe39fffdf6cc68dbe153167
Freya2597 <dsouza.f@northeastern.edu>
2022-12-11 13:01:37 -0500

    patient list and search method

--- VaccineDistributionSystem/src/userInterface/Patient/VaccinatedPatients.java
+++ VaccineDistributionSystem/src/userInterface/Patient/VaccinatedPatients.java
@@ -4,6 +4,15 @@
  */
 package userInterface.Patient;
 
+import TheBusiness.Enterprise.HospitalEnterprise;
+import TheBusiness.Organization.HospitalOrganization;
+import TheBusiness.Organization.Organization;
+import TheBusiness.Personnel.Patient;
+import TheBusiness.UserAccountManagement.UserAccount;
+import TheBusiness.VaccineManagement.AdministeredVaccineDetails;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Dsouza
@@ -11,11 +20,61 @@ package userInterface.Patient;
 public class VaccinatedPatients extends javax.swing.JPanel {
 
     /**
-     * Creates new form VaccinatedPatients
+     * Creates new form AdministeredVaccineListJPanel
      */
-    public VaccinatedPatients() {
+    private JPanel workContainer;
+    private HospitalEnterprise enterprise;
+    private UserAccount userAccount;
+    
+    
+    public VaccinatedPatients(JPanel workContainer,HospitalEnterprise enterprise, UserAccount userAccount ) {
         initComponents();
+        this.workContainer = workContainer;
+        this.enterprise = enterprise;
+        this.userAccount = userAccount;
+        populateTable();
     }
+    
+    
+    private void populateTable()
+    {
+        
+         int rowCount = tblvaccl.getRowCount();
+        DefaultTableModel defaulttabelmodel = (DefaultTableModel)tblvaccl.getModel();
+        
+        for(int i=rowCount-1 ; i>=0; i--){
+            defaulttabelmodel.removeRow(i);
+        }
+        Patient patient =(Patient)userAccount.getPerson();
+        
+        
+             for(Organization org: enterprise.getOrganizationDirectory().getOrganizationList())
+        {
+            if(org instanceof HospitalOrganization)
+            {
+                HospitalOrganization hospOrg = (HospitalOrganization)org;
+                for(AdministeredVaccineDetails vaccine: hospOrg.getClinic().getAdministeredVaccineList())
+                {     
+                    if(!(vaccine.getVaccineProduct()== null) && !(vaccine.getPatient()==null) && vaccine.getPatient().equals(patient))
+                    {Object[] row = new Object[9];
+                    row[0]=vaccine.getVaccineProduct();
+                    row[1]=vaccine;
+                    row[2]=vaccine.getVaccineProduct().getManufacturerName();
+                    row[3]=vaccine.getVaccineProduct().getBatchId();
+                    row[4]=vaccine.getVaccineProduct().getVaccineId();
+                    row[5]=vaccine.getDate();
+                    row[6]=vaccine.getSiteRoute();
+                    row[7]=vaccine.getDoseNumber();
+                    row[8]=vaccine.getshotstatus();
+                    defaulttabelmodel.addRow(row);
+                    
+                    }
+                }
+            }
+        }
+
+    }
+
 
     /**
      * This method is called from within the constructor to initialize the form.
